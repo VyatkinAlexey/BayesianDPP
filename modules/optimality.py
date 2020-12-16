a4aeb4e35bb877f0ad8eacf04f17d091aa99f4bc
Svetlana Gabdullina <ya.koroleva98@yandex.ru>
2020-12-16 17:01:15 +0300

    Optimality done

--- modules/optimality.py
+++ modules/optimality.py
@@ -36,34 +36,46 @@ def optimality_A(sigma: float, X_s: np.ndarray, A: np.ndarray) -> float:
     return np.float(np.trace(linalg.inv(to_invert)))
 
 
-def optimality_C(sigma: float, X_s: np.ndarray, A: np.ndarray) -> float:
+def optimality_C(sigma: float, X_s: np.ndarray, A: np.ndarray, c: np.array) -> float:
     """
-
+    
+    :param sigma: float, variance
     :param X_s: np.ndarray, matrix of feature subset
     :param A: np.ndarray, prior precision matrix
+    :param c: np.array, real vector
     :return: float, optimality value
     """
 
-    raise NotImplementedError()
+    Sigma = subset_covariance(X_s)
+    to_invert = Sigma + A
+    return np.float(c.T @ linalg.inv(to_invert) @ c)
+    
 
 def optimality_D(sigma: float, X_s: np.ndarray, A: np.ndarray) -> float:
     """
-
+    
+    :param sigma: float, variance
     :param X_s: np.ndarray, matrix of feature subset
     :param A: np.ndarray, prior precision matrix
     :return: float, optimality value
     """
 
-    raise NotImplementedError()
+    Sigma = subset_covariance(X_s)
+    to_invert = Sigma + A
+    return np.float(linalg.det(linalg.inv(to_invert)) ** (1 / A.shape[0]))
 
 
-def optimality_V(sigma: float, X_s: np.ndarray, A: np.ndarray) -> float:
+def optimality_V(sigma: float, X_s: np.ndarray, A: np.ndarray, X: np.ndarray) -> float:
     """
-
+    
+    :param sigma: float, variance
     :param X_s: np.ndarray, matrix of feature subset
     :param A: np.ndarray, prior precision matrix
+    :param X: np.ndarray, matrix of features
     :return: float, optimality value
     """
 
-    raise NotImplementedError()
+    Sigma = subset_covariance(X_s)
+    to_invert = Sigma + A
+    return np.float(np.trace(X @ linalg.inv(to_invert) @ X.T) / X.shape[0])
 
