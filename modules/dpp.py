d0ecd184e8ffbe361202a4c4a3651d36bee3a1df
Maxik-lun <max-lunitsin@mail.ru>
2020-12-16 19:22:37 +0300

    modify dpp.py and proposed.py

--- modules/dpp.py
+++ modules/dpp.py
@@ -1,1 +1,14 @@
-# TODO add implementation of DPP process there
+import numpy as np
+from dppy.finite_dpps import FiniteDPP
+
+def dpp(B):
+    '''
+    B is the (n,d)-matrix from the paper
+    '''
+    U,s,_ = np.linalg.svd(B)
+    eig_vals = np.zeros(len(U))
+    eig_vals[:len(s)] = np.abs(s)**2
+    DPP = FiniteDPP(kernel_type='correlation',
+                projection=False,
+                **{'K_eig_dec': (eig_vals, U)})
+    return DPP
--- selection_methods/proposed.py
+++ selection_methods/proposed.py
@@ -1,1 +1,36 @@
-# TODO add actual method from the article
+from typing import Tuple
+
+import numpy as np
+from scipy.linalg import fractional_matrix_power as frac_power
+from modules.utils import _get_optimalty
+from modules.dpp import dpp
+    
+
+def select_proposed(sigma: float,
+                     X: np.ndarray,
+                     A: np.ndarray,
+                     k: int,
+                     optimality: str = 'A') -> Tuple[np.ndarray, float]:
+    """
+
+    :param sigma: float, variance
+    :param X: np.ndarray, matrix of features
+    :param A: np.ndarray, prior precision matrix
+    :param k: int, number of samples to select
+    :param optimality: str, optimality type, one of ["A", "C", "D", "V"]
+    :return: indexes of samples subset
+    """
+    optimalty_func = _get_optimalty(optimality)
+    num_samples = X.shape[0]
+    assert num_samples >= k, f'number of samples should be greater than k'
+    p = k/num_samples * np.ones(num_samples)
+    Z = A + (X.T*p)@X
+    B = (np.sqrt(p[:,None])*X)@frac_power(Z,-0.5)
+    DPP = dpp(B)
+    DPP.sample_exact()
+    all_n = np.arange(num_samples)
+    b = set(all_n[np.random.uniform(size = num_samples) < p])
+    selected_ixs = set(DPP.list_of_samples[0]) | b
+    selected_ixs = np.array(list(selected_ixs))
+    optimality_value = optimalty_func(sigma, X[selected_ixs], A)
+    return selected_ixs, optimality_value
